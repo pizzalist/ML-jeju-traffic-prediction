aaa5ddafb04ae4e7492a5c8bb2f633f869590a85
noah <letgoofthepizza@gmail.com>
2022-07-30 23:02:41 +0900

    vue:study

---
+++ project/src/views/1_databinding/DataBindingAttributeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input type="text" v-bind:value="userId" readonly />
+    <input type="text" :value="userId" readonly />
+    <br />
+    <img :src="imgSrc" alt="" style="width: 100px; height: auto" />
+    <br />
+    <input type="search" name="" id="" v-model="txt1" />
+    <button :disabled="txt1 === ''">조회</button>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      userId: 'noah',
+      imgSrc: 'https://masteringjs.io/assets/images/vue/vue-transparent.png',
+      txt1: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
---
+++ project/src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Hello {{ userName }}</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      userName: 'Noah',
+      message: 'Welcome SME Bootcamp',
+      arr: [],
+      obj: {}
+    }
+  }
+}
+</script>
