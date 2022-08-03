c615b615171853f6d7b8563d9c10c10f4211c6c0
noah <letgoofthepizza@gmail.com>
2022-08-04 02:10:57 +0900

    Node.js: make & update  dataFile

--- Node.js/main.js
+++ Node.js/main.js
@@ -1,7 +1,36 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-const path = require('path');
+var qs = require('querystring');
+
+function templateHTML(title, list , body, control){
+    return `
+                    <!doctype html>
+                    <html>
+                    <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1><a href="/">WEB</a></h1>
+                        ${list}
+                        ${control}
+                        ${body}
+                    </body>
+                    </html>
+                    `;
+}
+
+function templateList(filelist){
+    var list = '<ul>';
+    var i =0;
+    while(i<filelist.length){
+        list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+        i=i+1;
+    }
+    list = list + '</ul>';
+    return list;
+}
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -10,59 +39,71 @@ var app = http.createServer(function(request,response){
 
         if(pathname === '/'){
             if(queryData.id === undefined){
-                fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
+                fs.readdir('./data', function(error, filelist){
+                    console.log(filelist);
                     var title = 'Welcome';
                     var description = 'Hello, fuckin Node.js';
-                    var template = `
-                    <!doctype html>
-                    <html>
-                    <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                    </head>
-                    <body>
-                    <h1><a href="/">WEB</a></h1>
-                    <ol>
-                        <li><a href="/?id=HTML">HTML</a></li>
-                        <li><a href="/?id=CSS">CSS</a></li>
-                        <li><a href="/?id=JavaScript">JavaScript</a></li>
-                    </ol>
-                    <h2>${title}</h2>
-                    <p>${description}</p>
-                    </body>
-                    </html>
-                    `;
+                    var list = templateList(filelist);
+                    var template = templateHTML(title, list, 
+                        `<h2>${title}</h2>${description}`,
+                        `<a href="/create">create</a>`
+                        );
                     response.writeHead(200);
                     response.end(template);
-                });
+                })
+
+                    
+
             }else{
+                fs.readdir('./data', function(error, filelist){
                 fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
                     var title = queryData.id;
-                    var template = `
-                    <!doctype html>
-                    <html>
-                    <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                    </head>
-                    <body>
-                    <h1><a href="/">WEB</a></h1>
-                    <ol>
-                        <li><a href="/?id=HTML">HTML</a></li>
-                        <li><a href="/?id=CSS">CSS</a></li>
-                        <li><a href="/?id=JavaScript">JavaScript</a></li>
-                    </ol>
-                    <h2>${title}</h2>
-                    <p>${description}</p>
-                    </body>
-                    </html>
-                    `;
+                    var list = templateList(filelist);
+                    var template = templateHTML(title, list, 
+                        `<h2>${title}</h2>${description}`,
+                        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`,
+                        );
                     response.writeHead(200);
                     response.end(template);
                 });
-            }
-            
-        } else{
+            });
+        }
+        }else if(pathname ==='/create'){
+            fs.readdir('./data', function(error, filelist){
+                    console.log(filelist);
+                    var title = 'WEB - create';
+                    var list = templateList(filelist);
+                    var template = templateHTML(title, list, `
+                        <form action="http://localhost:2000/create_process" method="post">
+                            <!-- method="post"  은밀하게 서버로 전송-->
+                            <p><input type="text" name="title" placeholder="title"></p>
+                            <p>
+                                <textarea name="description" placeholder="description"></textarea>
+                            </p>
+                            <p>
+                                <input type="submit">
+                            </p>
+                        </form>
+                    `, '');
+                    response.writeHead(200);
+                    response.end(template);
+                });
+        }else if(pathname ==='/create_process'){
+            var body = '';
+            request.on('data', function (data) {
+                body = body + data;
+            });
+            request.on('end', function () {
+                var post = qs.parse(body);
+                var title = post.title;
+                var description = post.description;
+                fs.writeFile(`data/${title}`, description, 'utf8', 
+                function(err){
+                    response.writeHead(302,{Location: `/?id=${title}`});
+                    response.end('success');    
+                })
+            });
+        }else{
                 response.writeHead(404);
                 response.end('Not found');
             }
